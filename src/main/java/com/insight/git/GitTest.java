63b9af956f81efbd1a5434182be4f64989df3aa0
insight <2695936421@qq.com>
2022-05-31 19:29:18 +0800

    second commit

--- src/main/java/com/insight/git/GitTest.java
+++ src/main/java/com/insight/git/GitTest.java
@@ -3,5 +3,6 @@ package com.insight.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git by Insight");
+        System.out.println("hello Git2 by Insight");
     }
 }
