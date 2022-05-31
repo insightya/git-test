dcad798b966086f06daeef030c4387bc4017cd4a
insight <2695936421@qq.com>
2022-05-31 19:36:10 +0800

    third commit

--- src/main/java/com/insight/git/GitTest.java
+++ src/main/java/com/insight/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git by Insight");
         System.out.println("hello Git2 by Insight");
+        System.out.println("hello Git3 by Insight");
     }
 }
